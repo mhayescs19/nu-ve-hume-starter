8b8ec6b4c3e54ed2e77c9cfe3760ee6ed653f51f
mhayescs19 <mhayescs19@gmail.com>
2024-10-01 22:59:15 -0700

    update dropdown and move light/dark mode dialog

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -15,7 +15,6 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 
-
 export const Nav = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
@@ -45,14 +44,35 @@ export const Nav = () => {
         <DropdownMenu>
           <DropdownMenuTrigger>Hamburger</DropdownMenuTrigger>
           <DropdownMenuContent>
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">user name</p>
+            <p className="text-xs leading-none text-muted-foreground">
+              me@nu-ve.com
+            </p>
+          </div>
+        </DropdownMenuLabel>
             <DropdownMenuSeparator />
             <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Settings</DropdownMenuItem>
+            <DropdownMenuItem>Support</DropdownMenuItem>
+            <DropdownMenuItem onClick={toggleDark}>
+                <span className="pr-2">{isDarkMode ? "Light" : "Dark"} Mode</span>
+                <span>
+                  {isDarkMode ? (
+                    <Sun className={"size-4"} />
+                  ) : (
+                    <Moon className={"size-4"} />
+                  )}
+                </span>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Log out</DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
       <div>
-        <HumeLogo className={"h-5 w-auto"} />
+        <HumeLogo className={"pl-3 h-5 w-auto"} />
       </div>
       <div className={"ml-auto flex items-center gap-1"}>
         <Button
